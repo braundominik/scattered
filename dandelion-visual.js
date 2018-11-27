cd357f7b4d71adbe0dc7196e42f213c852ec0e6c
Dominik <dominik.braun@hs-furtwangen.de>
2018-11-27 11:42:50 +0100

    working sample

---
+++ dandelion-visual.js
@@ -0,0 +1,31 @@
+var volumeTest;
+(function (volumeTest) {
+    window.addEventListener("load", init);
+    let crc;
+    let canvas;
+    let progress = 0.5;
+    function init() {
+        canvas = document.getElementById("flower");
+        crc = canvas.getContext("2d");
+        //animate();
+    }
+    /* function animate(): void {
+        draw();
+        setTimeout(animate, 100);
+    } */
+    function draw(volume) {
+        crc.fillStyle = "white";
+        crc.fillRect(0, 0, canvas.width, canvas.height);
+        crc.strokeStyle = "black";
+        crc.lineWidth = 20;
+        crc.beginPath();
+        if (volume > 0.01) {
+            progress = progress + 0.005;
+        }
+        crc.arc(200, 200, 100, progress * Math.PI, 2.5 * Math.PI);
+        crc.stroke();
+        console.log(volume);
+    }
+    volumeTest.draw = draw;
+})(volumeTest || (volumeTest = {}));
+//# sourceMappingURL=dandelion-visual.js.map
---
+++ dandelion-visual.ts
@@ -0,0 +1,37 @@
+namespace volumeTest {
+
+window.addEventListener("load", init);
+
+let crc: CanvasRenderingContext2D;
+let canvas: HTMLCanvasElement;
+
+let progress: number = 0.5;
+
+function init(): void {
+    canvas = <HTMLCanvasElement>document.getElementById("flower");
+    crc = canvas.getContext("2d");
+    //animate();
+
+}
+
+/* function animate(): void {
+    draw();
+    setTimeout(animate, 100);
+} */
+
+export function draw(volume): void {
+
+    crc.fillStyle = "white";
+    crc.fillRect(0, 0, canvas.width, canvas.height);
+    crc.strokeStyle = "black";
+    crc.lineWidth = 20;
+    crc.beginPath();
+    if(volume>0.01){
+        progress = progress + 0.005;
+    }
+    crc.arc(200, 200, 100, progress * Math.PI, 2.5 * Math.PI);
+    crc.stroke();
+    console.log(volume);
+}
+
+}
