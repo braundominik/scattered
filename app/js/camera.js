fc086a6c79c5078d6b92d8a4660de92c053c9f8f
braundominik <dominik.braun@hs-furtwangen.de>
2019-01-09 12:41:33 +0100

    framing

--- app/js/camera.js
+++ app/js/camera.js
@@ -111,7 +111,7 @@ function takepicture() {
         //context.drawImage(video, -50, 0, width, height);
         canvas.width = width;
         canvas.height = height;
-        context.drawImage(video, 0, diff, width, (height-diff));
+        context.drawImage(video, 0, diff*2, width, (height-diff));
 
         var data = canvas.toDataURL('image/png');
         //photo.setAttribute('src', data);
